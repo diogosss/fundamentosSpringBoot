63fb0bca3178874e42f0bb6d27a89f40580b156e
Diego Salazar <diogol11ldu20@hotmail.com>
2022-06-04 13:29:19 -0500

    [Lesson 11] Inyeccion de dependencia Component

--- src/main/java/com/fundamentos/springboot/fundamentos/FundamentosApplication.java
+++ src/main/java/com/fundamentos/springboot/fundamentos/FundamentosApplication.java
@@ -1,13 +1,30 @@
 package com.fundamentos.springboot.fundamentos;
 
+import com.fundamentos.springboot.fundamentos.component.ComponentDependency;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class FundamentosApplication {
+public class FundamentosApplication implements CommandLineRunner {
+
+	private ComponentDependency componentDependency; //inyectar la interfaz
+
+	//inyeccion de componentes en constructor
+	//cuando hay dos clases usando la interfaz ComponentDependency hay un error
+	//se solucuiona con Qualifier -> **la primerera letra en minuscula
+	public FundamentosApplication(@Qualifier("componentTwoImplement") ComponentDependency componentDependency){
+		this.componentDependency = componentDependency;
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(FundamentosApplication.class, args);
 	}
 
+	@Override
+	public void run(String... args) throws Exception {
+		//ejecutar en applicacion
+		componentDependency.saludar();
+	}
 }
